29628a74c01d2fb2e5a17dc7012dd5854759cbe0
Felipe Carlota <felipe.carlota@gmail.com>
2019-02-12 20:46:58 -0200

    Finalizado layout da home

--- index.php
+++ index.php
@@ -3,7 +3,21 @@
 
 <!-- o blog vai aqui :) -->
 
-
+<div id="cabecalho">
+  <div class="container">
+    <div class="row">
+    <div class="col-lg-12">
+      
+        <img src="img/logo_cabecalho.png" class="d-block w-100 img_logo">
+        <p>Últimos posts</p>
+        <h2>Fret SPOT: dicas para economizar com<br> transporte de emergência</h2>
+      <button type="button">
+        Continue...
+      </button>
+    </div>
+  </div>
+  </div>
+</div>
 
 
 
