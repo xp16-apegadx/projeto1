c9b45a4960de8a6944c82f65721e1b54549a5bc3
ernaneaugusto <ernaneatcipriano@gmail.com>
2019-02-13 04:32:53 -0200

    Add nova página

---
+++ cabine-automatica-multa-acima-40km.php
@@ -0,0 +1,73 @@
+<?php require_once("includes/header.php")?>
+
+<meta name="description" content="Novos radares nas cabines automáticas de pedágios de Coronel Macedo, Jaú e Boa Esperança do Sul, no sudoeste paulista, do tipo lombada eletrônica, já se encontram em operação desde a segunda-feira, 11!">
+  <title>Fique ligado! Cabine automática de pedágio multa motoristas a mais de 40km/h | Blog do Frete</title>
+</head>
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MZ9WB3V"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<header class="header-menu">        
+    <div class="container">
+        <div class="row py-3">
+            <div class="col-12">
+                <a href="http://blogdofrete.com.br/" title="Home Blog do Frete">
+                    <img src="/img/logo_cabecalho.png" class="img-logo" alt="Logo Blog do Frete" title="Logo Blog do Frete" />
+                </a>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section id="conteudo">
+  <div class="container">
+    <div class="row pt-5">
+
+      <div class="col-12 conteudo-post">
+        <div class="data mb-3">
+          <span class="destaque">multas</span>
+          <span class="data"> 13 de Fev 2019</span>
+        </div>    
+        
+        <h1 class="post-title text-center">Fique ligado! Cabine automática de pedágio multa motoristas a mais de 40km/h</h1>
+        <img src="http://blogdofrete.com.br/img/radares-em-cabines-automaticas-de-pedagio.jpg" class="panorama" title="Fique ligado! Cabine automática de pedágio multa motoristas a mais de 40km/h" alt="novos fretes" />
+        <h2>Novos <strong>radares</strong> nas cabines automáticas de <strong>pedágios</strong> de Coronel Macedo, Jaú e Boa Esperança do Sul, no sudoeste paulista, do tipo lombada eletrônica, já se encontram em operação desde a segunda-feira, 11.</h2>
+ 
+        <p>Foram instalados, também, em oito praças de <strong>pedágios</strong> da região de Marília. Estes ainda aguardando autorização do Departamento de Estradas de Rodagem (DER) para iniciar a operação.</p>
+        
+        <p>Na pista de <strong>cobrança automática</strong>, os motoristas não param para realizar o pagamento, pois a cobrança é feita remotamente pela utilização de um dispositivo eletrônico instalado no veículo que libera a passagem.</p>
+        
+        <p>A <strong>fiscalização</strong> é uma exigência dos contratos mais recentes de concessão de rodovias, afirma a Agência de Transportes do Estado de São Paulo (Artesp). Tal medida surgiu com a finalidade de aumentar a segurança dos funcionários nas <strong>cabines de pagamento eletrônico</strong> e dos usuários, pois o <strong>excesso de velocidade</strong> tem causado acidentes quando, por exemplo, o pagamento não é autorizado e a sirene apita para o veículo aumentando o risco de colisão traseira, por exemplo.</p>
+        
+        <p>Segundo o Código de Trânsito Brasileiro, o motorista que passar com velocidade acima dos 40 km/h em até 20% será multado por infração média. Quando ultrapassado um limite entre 20% e 50%, a <strong>multa</strong> é grave, tornando-se gravíssima em velocidade acima de 50% do permitido.</p>
+
+        <h3>Lombadas eletrônicas em operação:</h3>
+        <ul>
+            <li>Jaú – Rodovia Comandante João Ribeiro de Barros, km 156,6 (quatro)</li>
+            <li>Coronel Macedo – Rodovia Jurandir Siciliano, km 331,5 (quatro)</li>
+            <li>Boa Esperança do Sul – Rodovia Com. João Ribeiro de Barros, km 117, 2 (quatro)</li>
+        </ul>
+        
+        <h3>Pedágios com lombadas à espera de homologação:</h3>
+        <ul>
+            <li>Ituverava – SP-300, km 405</li>
+            <li>Sales Oliveira – SP-300, km 350,1</li>
+            <li>Sertãozinho – SP-322, km 327,5</li>
+            <li>Pitangueira – SP-322, km 361,4</li>
+            <li>Pongaí – SP-333, km 234,5</li>
+            <li>Marília – SP-333, km 315,1</li>
+            <li>Echaporã – SP-333, km 354,7</li>
+            <li>Florínea – SP-333, km 447,5</li>
+        </ul>
+
+          <div class="separador"></div>          
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php 
+require_once("includes/footer.php");
+?>
