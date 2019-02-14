e216aadf9953fa0f69350ea19bbc570b56dd54b6
Felipe Carlota <felipe.carlota@gmail.com>
2019-02-14 20:40:37 -0200

    Criado post e imagem referente ao preço da gasolina.

---
+++ voce-sabia-que-o-bolsonaro-pode-ter-influenciado-no-preco-da-gasolina.php
@@ -0,0 +1,96 @@
+<?php require_once("includes/header.php")?>
+
+<meta name="description" content="Saiba aqui como Bolsonaro pode ter influenciado no preço da gasolina neste início de 2019, clique e confira!">
+  <title>Você sabia que o Bolsonaro pode ter influenciado no preço da gasolina? | Blog do Frete</title>
+</head>
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MZ9WB3V"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<header class="header-menu">        
+    <div class="container">
+        <div class="row py-3">
+            <div class="col-12">
+                <a href="http://blogdofrete.com.br/" title="Home Blog do Frete">
+                    <img src="img/logo_cabecalho.png" class="img-logo" alt="Logo Blog do Frete" title="Logo Blog do Frete" />
+                </a>
+            </div>
+        </div>
+    </div>
+</header>
+<section id="conteudo">
+  <div class="container">
+    <div class="row pt-5">
+
+      <div class="col-12 conteudo-post">
+        <div class="data mb-3">
+          <span class="destaque">notícias</span>
+          <span class="data"> 14 de Fev 2019</span>
+        </div>    
+        
+        <h1 class="post-title text-center">ATENÇÃO MOTORISTA! Você sabia que o Bolsonaro pode ter influenciado no preço da gasolina?</h1>
+        <img src="http://blogdofrete.com.br/img/voce-sabia-que-o-bolsonaro-pode-ter-influenciado-no-preço-da-gasolina.jpg" class="panorama" title="Você sabia que o Bolsonaro pode ter influenciado no preço da gasolina?" alt="bolsonaro e a gasolina" />
+      
+            <h2><em>É indiscutível que estamos em tempos de guerras políticas, mas ainda assim, os benefícios que o governo Bolsonaro vem trazendo, principalmente em seu estágio inicial, já são visíveis em todo o Brasil.</em></h2>
+            
+            <p>2018 foi um ano onde a polaridade política estava aflorada em todos nós brasileiros. De um lado, uma esquerda já enfraquecida tentava de todas as formas eleger um “antigo messias”, do outro, uma direita que já estava acumulando forças desde os movimentos populares em 2014, com seu “mito salvador”. E o ano estava só começando...</p>
+
+            <p>Em maio, um movimento popular de caminhoneiros e motoristas autônomos que já vinha se organizando, criou a força necessária para então, barrar o inviavel <strong>aumento no preço do combustível</strong>. O resultado? O Brasil num caos durante 9 dias consecutivos, algumas cidades declarando estado de calamidade pública, falta de alimentos, medicamentos e água nas prateleiras dos supermercados e farmácias e muito mais.</p>
+
+            <h2>Mas fica a pergunta: “o que raios o Bolsonaro tem a ver com isso?”</h2>
+
+            <p>Bom, desde sua eleição, diversas saídas estão sendo repensadas para que, diferentemente do último governo, atitudes como as da <strong>greve dos caminhoneiros</strong> não voltem a acontecer, vejam só:</p>
+
+            <h3>1º Equipe de Bolsonaro avalia imposto flexível para combustível</h3>
+
+            <p>Para contornar os erros do governo passado, a equipe econômica do Bolsonaro busca a criação de um imposto flexível: nas altas do preço do petróleo, o imposto seria reduzido e na baixa, voltaria à sua alíquota.</p>
+
+            <p>Um modelo já adotado em diversos países da Europa.</p>
+
+            <p>Fonte:<br>
+            <a href="https://www1.folha.uol.com.br/mercado/2018/10/equipe-de-bolsonaro-avalia-imposto-flexivel-para-combustivel.shtml">https://www1.folha.uol.com.br/mercado/2018/10/equipe-de-bolsonaro-avalia-imposto-flexivel-para-combustivel.shtml</a></p>
+
+            <h3>2º Gasolina atinge o menor preço nas refinarias nos últimos 15 meses</h3>
+
+            <p>Desde 2017 a gasolina não era comercializada a R$1,45 nas refinarias. E para César Bergo, especialista em mercado financeiro da Universidade de Brasília (UnB), a redução no preço é o resultado da lua-de-mel com o novo governo e do cenário internacional.</p>
+
+            <p>A <strong>redução do preço do combustível</strong> foi mais presente no DF.</p>
+
+            <p>Fonte:<br>
+            <a href="https://www.correiobraziliense.com.br/app/noticia/economia/2019/01/04/internas_economia,728850/gasolina-atinge-o-menor-preco-nas-refinarias-nos-ultimos-15-meses.shtml">https://www.correiobraziliense.com.br/app/noticia/economia/2019/01/04/internas_economia,728850/gasolina-atinge-o-menor-preco-nas-refinarias-nos-ultimos-15-meses.shtml</a></p>
+
+            <h3>3º Postos do DF reduzem preço da gasolina para comemorar posse de Bolsonaro</h3>
+
+            <p>Preço caiu de R$ 4,29 para R$ 3,89. Faixas de promoção deram boas-vindas ao novo presidente. Segundo o chefe de pista do posto Jajour da Asa Norte, Francisco Peres, a promoção foi uma forma de acolher o presidente e os turistas que estão em Brasília para a acompanhar a posse desta terça-feira (1º).</p>
+
+            <p>Fonte:<br>
+            <a href="https://g1.globo.com/df/distrito-federal/noticia/2018/12/31/postos-do-df-reduzem-preco-da-gasolina-para-comemorar-posse-de-bolsonaro.ghtml">https://g1.globo.com/df/distrito-federal/noticia/2018/12/31/postos-do-df-reduzem-preco-da-gasolina-para-comemorar-posse-de-bolsonaro.ghtml</a></p>
+
+            <h2>Soluções para motoristas e caminhoneiros autônomos</h2>
+
+            <p>Além disso, para nós, motoristas e <strong>caminhoneiros autônomos</strong>, novas medidas para o frete estão sendo tomadas, como a extinção do tabelamento, visando uma antecipação à futuros problemas.</p>
+
+            <p>Fonte:<br>
+            <a href="https://veja.abril.com.br/economia/ao-falar-em-frete-bolsonaro-diz-que-e-melhor-se-antecipar-a-problemas/">https://veja.abril.com.br/economia/ao-falar-em-frete-bolsonaro-diz-que-e-melhor-se-antecipar-a-problemas/</a></p>
+
+            <p>Desta forma, a equipe técnica do presidente eleito, também discute a criação de um fundo de compensação para evitar flutuação excessiva no preço de combustíveis.</p>
+
+            <p>Fonte:<br>
+            <a href="https://www.institutomillenium.org.br/blog/equipe-de-bolsonaro-ja-discute-nova-tabela-de-frete-para-evitar-greve-de-caminhoneiros/">https://www.institutomillenium.org.br/blog/equipe-de-bolsonaro-ja-discute-nova-tabela-de-frete-para-evitar-greve-de-caminhoneiros/</a></<p>
+
+            <p>2019 ainda está só começando, mas tão breve já podemos avistar os avanços proporcionados pelo novo governo. Será que ele conseguirá evitar que novas frentes populares, criadas pelos <strong>motoristas e caminhoneiros autônomos</strong> voltem a criar força pelo Brasil?</p>
+
+            <p>Isso, só o tempo dirá.</p>
+
+
+          <div class="separador"></div>          
+      </div>
+    </div>
+  </div>
+</section>
+<?php 
+// require_once("includes/form-cadastro.php");
+require_once("includes/footer.php");
+?>
