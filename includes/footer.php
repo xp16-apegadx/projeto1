661b8b45f51cf13416764975ab775d3cabbb5905
ernaneaugusto <ernaneatcipriano@gmail.com>
2019-02-13 23:07:43 -0200

    Add script de compartilhamento redes sociais

--- includes/footer.php
+++ includes/footer.php
@@ -179,6 +179,10 @@
     });
 </script>
 <!-- fim script do firebase -->
-
+<script>
+  (function (w,i,d,g,e,t,s) {w[d] = w[d]||[];t= i.createElement(g);
+    t.async=1;t.src=e;s=i.getElementsByTagName(g)[0];s.parentNode.insertBefore(t, s);
+  })(window, document, '_gscq','script','//widgets.getsitecontrol.com/175470/script.js');
+</script>
 </body>
 </html> 
